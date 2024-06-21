ef0bdb14a5112c6fe5dc4a1eb5acf6e8e4af7753
Himanshut0012 <himanshut0012@gmail.com>
2024-06-21 20:27:45 +0530

    repo-custom

--- DocGenLib/src/main/java/com/docGenSvc/config/JpaConfig.java
+++ DocGenLib/src/main/java/com/docGenSvc/config/JpaConfig.java
@@ -38,7 +38,7 @@ public class JpaConfig {
 
     private Properties jpaProperties(){
         Properties properties = new Properties();
-        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
         properties.setProperty("hibernate.hbm2ddl.auto", "update");
         properties.setProperty("hibernate.show_sql", "true");
         properties.setProperty("hibernate.format_sql", "true");
